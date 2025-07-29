6d4f393eb5f0c0b44d066c266b09abe5a8dee7db
Satvik <satvikramanujam@gmail.com>
2025-07-29 17:39:26 +0530

    done

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ print("✅ .env loaded:", loaded)  # debug
 MONGO_URL = os.getenv("MONGO_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 300))
